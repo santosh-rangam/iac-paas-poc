a136fa0e47ecdb5ae999d0f5b9bc6630f5168461
santosh-rangam <rangamdhamani@gmail.com>
2016-01-29 10:21:25 -0500

    initial commit comment

---
+++ app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hellow World');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port);
+});
+var redis = require("redis");
+
+// Add your cache name and access key.
+var client = redis.createClient(6379,'iacredis.redis.cache.windows.net', {auth_pass: 'o5f1Taf2OjBQFJJASkLk5UCIjp0rBtoIDDYz6300fBY=' });
+
+client.set("foo", "bar", function(err, reply) {
+    console.log(reply);
+});
+
+client.get("foo",  function(err, reply) {
+    console.log(reply);
+});
